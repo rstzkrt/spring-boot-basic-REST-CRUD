e394115bb493245a3dcb0caf844f7dd0d7a9476d
Resat Ozkurt <ozkurtresat@gmail.com>
2021-05-05 22:46:28 +0200

    project scaffold created

---
+++ src/main/java/com/example/basiccrudrest/BasicCrudRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.basiccrudrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrudRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicCrudRestApplication.class, args);
+    }
+
+}
